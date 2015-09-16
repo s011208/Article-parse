f612d2515ffa41163af46ac39f1a0bb55a209e8e
Yen-Hsun, Huang <s011208@gmail.com>
2015-09-16 12:27:53 +0800

    set limitation & refactor indent

--- cloud/main.js
+++ cloud/main.js
@@ -79,73 +79,73 @@ function contains(originStr, txt) {
 
 // +++ class
 var Monster = Parse.Object.extend("Monster", {
-                                  // Instance methods
-                                  hasSuperHumanStrength: function () {
-                                  return this.get("strength") > 18;
-                                  },
-                                  // Instance properties go in an initialize method
-                                  initialize: function (attrs, options) {
-                                  this.sound = "Rawr"
-                                  }
-                                  }, {
-                                  // Class methods
-                                  spawn: function(strength) {
-                                  var monster = new Monster();
-                                  monster.set("strength", strength);
-                                  return monster;
-                                  }
-                                  })
+	  // Instance methods
+		  hasSuperHumanStrength: function () {
+			  return this.get("strength") > 18;
+		  },
+		  // Instance properties go in an initialize method
+		  initialize: function (attrs, options) {
+			  this.sound = "Rawr"
+		  }
+	  }, {
+	  // Class methods
+		  spawn: function(strength) {
+			  var monster = new Monster();
+			  monster.set("strength", strength);
+			  return monster;
+		  }
+	})
 ;
 
 var JobRange = Parse.Object.extend("ActListItem_Job", {
-		/*instance method*/
-		getJobBound: function() {
-			return this.get("job_bound");
-		},
-		getStartIndex: function() {
-			return this.get("start_index");
-		}
-	}, {
-	/*class method*/
-		newInstance: function(jobBound, startIndex) {
-			var item = new JobRange();
-			item.set("job_bound", jobBound);
-			item.set("start_index", startIndex);
+			/*instance method*/
+			getJobBound: function() {
+				return this.get("job_bound");
+			},
+			getStartIndex: function() {
+				return this.get("start_index");
+			}
+		}, {
+		/*class method*/
+			newInstance: function(jobBound, startIndex) {
+				var item = new JobRange();
+				item.set("job_bound", jobBound);
+				item.set("start_index", startIndex);
 			return item;
 		}
 	})
 ;
 
 var ActListItem = Parse.Object.extend("ActListItem_test", {
-                                      /*instance method*/
-										  getAmendedDate: function() {
-											return this.get("amended_date");
-										  },
-										  
-										  getCategoty: function () {
-											return this.get("category_");
-										  },
-										  
-										  getTitle: function () {
-											return this.get("title_");
-										  },
-										  
-										  getUrl: function (){
-											return this.get("url");
-										  }
-                                      
-                                      }, {
-                                      /*class method*/
-										  newInstance: function(category, lawUrl, title, amendedDate) {
-											  var item = new ActListItem();
-											  item.set("amended_date", amendedDate);
-											  item.set("category_", category);
-											  item.set("title_", title);
-											  item.set("url", lawUrl);
-											  return item;
-										  }
-                                      }
-                                      )
+			/*instance method*/
+			getAmendedDate: function() {
+				return this.get("amended_date");
+			},
+
+			getCategoty: function () {
+				return this.get("category_");
+			},
+
+			getTitle: function () {
+				return this.get("title_");
+			},
+
+			getUrl: function (){
+				return this.get("url");
+			}
+
+		}, {
+			/*class method*/
+			newInstance: function(category, lawUrl, title, amendedDate) {
+				var item = new ActListItem();
+				item.set("amended_date", amendedDate);
+				item.set("category_", category);
+				item.set("title_", title);
+				item.set("url", lawUrl);
+				return item;
+			}
+		}
+	)
 ;
 
 // --- class
@@ -179,27 +179,26 @@ function startToParseActItemList(job, status) {
 }
 
 Parse.Cloud.job("ParseAllActList", function(request, status) {
-                Parse.Cloud.useMasterKey();
-                var query = new Parse.Query(Parse.Object.extend("ActListItem_Job"));
-                query.get(ActListItem_Job_ID, {
-                          success: function(job) {
-                          startToParseActItemList(job, status);
-                          },
-                          error: function(object, error) {
-                          alert("query failed, msg: " + error.message);
-                          }
-                });
-
+	Parse.Cloud.useMasterKey();
+	var query = new Parse.Query(Parse.Object.extend("ActListItem_Job"));
+		query.get(ActListItem_Job_ID, {
+			success: function(job) {
+				startToParseActItemList(job, status);
+		},
+			error: function(object, error) {
+				alert("query failed, msg: " + error.message);
+		}
+	});
 })
 
 function setResult(promise, status) {
-    promise
-    .then(function() {
-          status.success("ParseAllActList success");
-          }, function(error){
-          status.error("ParseAllActList error: " + error);
-          }
-          );
+	promise
+	.then(function() {
+			status.success("ParseAllActList success");
+		}, function(error){
+			status.error("ParseAllActList error: " + error);
+		}
+	);
 }
 
 var toType = function(obj) {
@@ -207,130 +206,133 @@ var toType = function(obj) {
 }
 
 function getAllLawList(promise, linksList, status, job) {
-    var actItemsList = [];
-    var finishCounter = 0;
-    var totalActItemCounter = 0;
-    var startBound = job.getStartIndex();
-    var jobBound = job.getJobBound();
-    var endBound = startBound + job.getJobBound();
-    var updateStartBound = endBound;
-    if (endBound >= linksList.length) {
-        endBound = linksList.length;
-        updateStartBound = 0;
-    }
+	var actItemsList = [];
+	var finishCounter = 0;
+	var totalActItemCounter = 0;
+	var startBound = job.getStartIndex();
+	var jobBound = job.getJobBound();
+	var endBound = startBound + job.getJobBound();
+	var updateStartBound = endBound;
+	if (endBound >= linksList.length) {
+		endBound = linksList.length;
+		updateStartBound = 0;
+	}
 	var finalJobBound = endBound - startBound;
-    
-    
-    // update job bound
-    
-//    job.save(null, {
-//             success: function(job) {
-//                    job.set("start_index", updateStartBound);
-//                    job.save();
-//                }
-//             });
-			 
-    
+
+
+	// update job bound
+
+	job.save(null, {
+		success: function(job) {
+			job.set("start_index", updateStartBound);
+			job.save();
+		}
+	});
+
+
     log("getAllLawList, linksList size: " + linksList.length + ", startBound: " + startBound + ", endBound: " + endBound + ", finalJobBound: " + finalJobBound);
     for(i = startBound; i < endBound; i++) {
-        var link = linksList[i];
+		var link = linksList[i];
         Parse.Promise.as(i)
-        .then(function(i){
-              return Parse.Promise.as(i)
-              .then(function(i){
-                    var httpPromise = Parse.Promise.as();
-                    httpPromise
-                    .then(function() {return getHttpRequest(link, "getLawWebPage");})
-                    .then(function(pageData) {
-                          $ = cheerio.load(pageData.text);
-                          var eleTitleRaw = $("div.classtitle ul li");
-                          if (eleTitleRaw != null && eleTitleRaw.length > 0) {
-							  // get all items' value
-							  var eleTitle = $(eleTitleRaw[0]).text();
-							  var eleLaws = $("a[href][title]");
-							  var actItemList = [];
-							  for (j = 0; j < eleLaws.length; j++) {
-								  var lawUrl = $(eleLaws[j]).attr("href");
-								  if (contains(lawUrl, "PCode")) {
-									  ++totalActItemCounter;
-									  lawUrl = "http://law.moj.gov.tw/LawClass/LawContent.aspx?" + lawUrl.substr(lawUrl.indexOf("PCode"));
-									  var title = $(eleLaws[j]).attr("title");
-									  var nodeParent = $(eleLaws[j]).parent();
-									  $(eleLaws[j]).remove();
-									  var amendedDate = nodeParent.text().trim();
-									  
-									  // update or insert
-									  var actItem = ActListItem.newInstance(eleTitle, lawUrl, title, amendedDate);
-									  // log("actItem: " + actItem.getAmendedDate() + ", " + actItem.getCategoty() + ", " + actItem.getTitle() + ", " + actItem.getUrl());
-									  actItemList.push(actItem);
-								  }
-							  }
-							  Parse.Object.saveAll(actItemList).then(
-								  function(actItems) {
-									  return actItems;
-								  },
-								  function(error) {
-								      return [];
-							  })
-							  .then(function(savedActItemResults) {
-											if (savedActItemResults.length == 0) {
-												--finalJobBound;
-												if (finalJobBound == 0) {
-													log("finishCounter: " + finishCounter + ", totalActItemCounter: " + totalActItemCounter);
-													setResult(promise, status);
-												}
-											} else {
-												var query = new Parse.Query(Parse.Object.extend("ActListItem_test"));
-												query.equalTo("category_", savedActItemResults[0].getCategoty());
-												query.find()
-												.then(
-													// find duplicated items
-													function(parseActItemResults) {
-														var rtn = [];
-														for(j = 0; j < savedActItemResults.length; j++) {
-															var savedActItem = savedActItemResults[j];
-															for(i = 0; i <parseActItemResults.length; i++) {
-																var parseActItem = parseActItemResults[i];
-																if(parseActItem.getTitle() != savedActItem.getTitle()) continue;
-																if(parseActItem.id == savedActItem.id) continue;
-																rtn.push(parseActItem);
-															}
-														}
-														// return item to delete
-                                                      if(rtn.length > 0) {
-                                                      log("delete item length: " + rtn.length + ", category: " + rtn[0].getCategoty());
-                                                      }
-														return rtn;
-													},
-													function(error) {
-														return [];
-													}
-												)
-												.then(function(rtnParseActItemResults) {
-													log("rtnParseActItemResults item length: " + rtnParseActItemResults.length);
-                                                    var promises = [];
-													for (y = 0; y < rtnParseActItemResults.length; y++) {
-//														log("delete object id: " + rtnParseActItemResults[y].id);
-                                                        promises.push(rtnParseActItemResults[y].destroy());
-													}
-													return Parse.Promise.when(promises);
-												})
-												.then(function(emptyResults) {
-													--finalJobBound;
-													if (finalJobBound == 0) {
-														log("finishCounter: " + finishCounter + ", totalActItemCounter: " + totalActItemCounter);
-														setResult(promise, status);
-													}
-												});
-											}
-											
-										});
-                          }
-                          })
-                    ;
-              });
-        });
-    }
+		.then(function(i) {
+			return Parse.Promise.as(i)
+			.then(function(i) {
+				var httpPromise = Parse.Promise.as();
+				httpPromise
+				.then(function() {
+					return getHttpRequest(link, "getLawWebPage");
+				})
+				.then(function(pageData) {
+					$ = cheerio.load(pageData.text);
+					var eleTitleRaw = $("div.classtitle ul li");
+					if (eleTitleRaw != null && eleTitleRaw.length > 0) {
+						// get all items' value
+						var eleTitle = $(eleTitleRaw[0]).text();
+						var eleLaws = $("a[href][title]");
+						var actItemList = [];
+						for (j = 0; j < eleLaws.length; j++) {
+							var lawUrl = $(eleLaws[j]).attr("href");
+							if (contains(lawUrl, "PCode")) {
+								++totalActItemCounter;
+								lawUrl = "http://law.moj.gov.tw/LawClass/LawContent.aspx?" + lawUrl.substr(lawUrl.indexOf("PCode"));
+								var title = $(eleLaws[j]).attr("title");
+								var nodeParent = $(eleLaws[j]).parent();
+								$(eleLaws[j]).remove();
+								var amendedDate = nodeParent.text().trim();
+
+								// update or insert
+								var actItem = ActListItem.newInstance(eleTitle, lawUrl, title, amendedDate);
+								// log("actItem: " + actItem.getAmendedDate() + ", " + actItem.getCategoty() + ", " + actItem.getTitle() + ", " + actItem.getUrl());
+								actItemList.push(actItem);
+							}
+						}
+						Parse.Object.saveAll(actItemList)
+						.then(
+							function(actItems) {
+								return actItems;
+							},
+							function(error) {
+								return [];
+							}
+						)
+						.then(function(savedActItemResults) {
+						if (savedActItemResults.length == 0) {
+						--finalJobBound;
+							if (finalJobBound == 0) {
+								log("finishCounter: " + finishCounter + ", totalActItemCounter: " + totalActItemCounter);
+								setResult(promise, status);
+							}
+						} else {
+							var query = new Parse.Query(Parse.Object.extend("ActListItem_test"));
+							query.equalTo("category_", savedActItemResults[0].getCategoty());
+							query.limit(1000);
+							query.find()
+							.then(/* find duplicated items */
+								function(parseActItemResults) {
+									var rtn = [];
+									for(j = 0; j < savedActItemResults.length; j++) {
+										var savedActItem = savedActItemResults[j];
+										for(i = 0; i <parseActItemResults.length; i++) {
+											var parseActItem = parseActItemResults[i];
+											if(parseActItem.getTitle() != savedActItem.getTitle()) continue;
+											if(parseActItem.id == savedActItem.id) continue;
+											rtn.push(parseActItem);
+										}
+									}
+									// return item to delete
+									if(rtn.length > 0) {
+										log("delete item length: " + rtn.length + ", category: " + rtn[0].getCategoty());
+									}
+									return rtn;
+								},
+								function(error) {
+									return [];
+								}
+							)
+							.then(function(rtnParseActItemResults) {
+									log("rtnParseActItemResults item length: " + rtnParseActItemResults.length);
+									var promises = [];
+									for (y = 0; y < rtnParseActItemResults.length; y++) {
+									//	log("delete object id: " + rtnParseActItemResults[y].id);
+										promises.push(rtnParseActItemResults[y].destroy());
+									}
+									return Parse.Promise.when(promises);
+								}
+							)
+							.then(function(emptyResults) {
+									--finalJobBound;
+									if (finalJobBound == 0) {
+										log("finishCounter: " + finishCounter + ", totalActItemCounter: " + totalActItemCounter);
+										setResult(promise, status);
+									}
+								});
+							}
+						});
+					}
+				});
+			});
+		});
+	}
 }
 
 
@@ -338,35 +340,35 @@ function getAllLawClassNList(pageData) {
 //    log("start getAllLawClassNList with data:");
 //    log(pageData);
 //    log("pageData length: " + pageData.text.length);
-    $ = cheerio.load(pageData.text);
-    links = $('a'); //jquery get all hyperlinks
-    log("link length: " + links.length);
-    var classNLinks = [];
-    $(links).each(function(i, link) {
-                  var actText = $(link).text();
-                  var linkUrl = $(link).attr('href');
-                  var isNull = linkUrl == null;
-                  if (!isNull && startsWith(linkUrl, "LawClassListN.aspx?")) {
-                    classNLinks[classNLinks.length] = "http://law.moj.gov.tw/LawClass/" + linkUrl;
-                  //  log("links: " + classNLinks[classNLinks.length - 1]);
-                  }
-                });
-    return classNLinks;
+	$ = cheerio.load(pageData.text);
+	links = $('a'); //jquery get all hyperlinks
+	log("link length: " + links.length);
+	var classNLinks = [];
+	$(links).each(function(i, link) {
+		var actText = $(link).text();
+		var linkUrl = $(link).attr('href');
+		var isNull = linkUrl == null;
+		if (!isNull && startsWith(linkUrl, "LawClassListN.aspx?")) {
+			classNLinks[classNLinks.length] = "http://law.moj.gov.tw/LawClass/" + linkUrl;
+			//  log("links: " + classNLinks[classNLinks.length - 1]);
+		}
+	});
+	return classNLinks;
 }
 
 function getHttpRequest(link, logTag) {
-//    log("start " + logTag + " with link: " + link);
-    return Parse.Cloud.httpRequest({
-                                   url: link,
-                                   success: function(httpResponse) {
-//                                   log("[" + logTag + "] request success, httpResponse: " + httpResponse.text.substr(0, 30));
-                                   },
-                                   error: function(httpResponse) {
-                                   var status = httpResponse.status;
-                                   var text = httpResponse.text;
-                                   log("[" + logTag + "] request error, status:" + status);
-                                   }
-                                   });
+	//    log("start " + logTag + " with link: " + link);
+	return Parse.Cloud.httpRequest({
+		url: link,
+		success: function(httpResponse) {
+		//                                   log("[" + logTag + "] request success, httpResponse: " + httpResponse.text.substr(0, 30));
+		},
+		error: function(httpResponse) {
+			var status = httpResponse.status;
+			var text = httpResponse.text;
+			log("[" + logTag + "] request error, status:" + status);
+		}
+	});
 }
 
 
